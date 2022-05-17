9e30d3cd294c2dce8ab377889c4a0b21e3ab378a
tony <ayakovlenko@users.noreply.github.com>
2022-05-18 00:47:50 +0300

    add TakeWhile and DropWhile

--- itertools.go
+++ itertools.go
@@ -17,3 +17,29 @@ func Map[A any, B any](aa []A, mapper func(a A) B) []B {
 	}
 	return mapped
 }
+
+// DropWhile
+// https://docs.python.org/3/library/itertools.html#itertools.dropwhile
+func DropWhile[A any](aa []A, pred func(a A) bool) []A {
+	i := 0
+	for ; i < len(aa); i++ {
+		if !pred(aa[i]) {
+			break
+		}
+	}
+
+	return aa[i:]
+}
+
+// TakeWhile
+// https://docs.python.org/3/library/itertools.html#itertools.takewhile
+func TakeWhile[A any](aa []A, pred func(a A) bool) []A {
+	i := 0
+	for ; i < len(aa); i++ {
+		if !pred(aa[i]) {
+			break
+		}
+	}
+
+	return aa[:i]
+}
--- itertools_test.go
+++ itertools_test.go
@@ -34,3 +34,75 @@ func TestMap(t *testing.T) {
 		assert.Equal(t, want, have)
 	})
 }
+
+func TestDropWhile(t *testing.T) {
+
+	t.Run("DropWhile 1", func(t *testing.T) {
+		xs := []int{1, 4, 6, 4, 1}
+
+		want := []int{6, 4, 1}
+		have := DropWhile(xs, func(x int) bool {
+			return x < 5
+		})
+
+		assert.Equal(t, want, have)
+	})
+
+	t.Run("DropWhile 2", func(t *testing.T) {
+		xs := []int{1, 4, 6, 4, 1}
+
+		want := []int{}
+		have := DropWhile(xs, func(x int) bool {
+			return x < 10
+		})
+
+		assert.Equal(t, want, have)
+	})
+
+	t.Run("DropWhile 3", func(t *testing.T) {
+		xs := []int{1, 4, 6, 4, 1}
+
+		want := xs
+		have := DropWhile(xs, func(x int) bool {
+			return x < 1
+		})
+
+		assert.Equal(t, want, have)
+	})
+}
+
+func TestTakeWhile(t *testing.T) {
+
+	t.Run("TakeWhile 1", func(t *testing.T) {
+		xs := []int{1, 4, 6, 4, 1}
+
+		want := []int{1, 4}
+		have := TakeWhile(xs, func(x int) bool {
+			return x < 5
+		})
+
+		assert.Equal(t, want, have)
+	})
+
+	t.Run("TakeWhile 2", func(t *testing.T) {
+		xs := []int{1, 4, 6, 4, 1}
+
+		want := []int{}
+		have := TakeWhile(xs, func(x int) bool {
+			return x < 1
+		})
+
+		assert.Equal(t, want, have)
+	})
+
+	t.Run("TakeWhile 3", func(t *testing.T) {
+		xs := []int{1, 4, 6, 4, 1}
+
+		want := xs
+		have := TakeWhile(xs, func(x int) bool {
+			return x < 10
+		})
+
+		assert.Equal(t, want, have)
+	})
+}
